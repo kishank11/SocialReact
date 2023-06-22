e9016b2087610bbb9ab0570d186011987a11da4d
kishank11 <kishankanojia11@gmail.com>
2023-06-22 13:56:40 +0530

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/home";
-
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 function App() {
   return (
-    <Home />
+    <Register />
   );
 }
 
--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,14 @@
+import { Posts } from "../../dummyData";
+import Post from "../post/Post";
+import Share from "../share/Share";
 import "./feed.css";
 export default function Feed() {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <Share />
+      {Posts.map((p) => (
+        <Post key={p.id} post={p} />
+      ))}
+    </div>
+  );
 }
